b3c9948b268aa646729ceefd1e3233f5a2c82bd7
abners <peng731647424@gmail.com>
2016-12-30 17:30:20 +0800

    删除邮件时添加多线程支持，提高删除效率

--- src/MailDelete.java
+++ src/MailDelete.java
@@ -13,6 +13,8 @@ import javax.mail.Store;
 import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 类MailDelete.java的实现描述：
@@ -24,23 +26,31 @@ public class MailDelete {
     public static void delete(String user, String password, String keyword, String folderName)
                                                                                               throws UnsupportedEncodingException {
         MessageSendService messageSendService = new EmailSendServicImpl();
-        try {
-            StoryFactory factory = new IMAPStoryFactory();
-            Store store = factory.getInstance();
+        int j = 0;
+        while (j++ < 10) {
+            try {
+                StoryFactory factory = new IMAPStoryFactory();
+                Store store = factory.getInstance();
 
-            IMAPFolder emailFolder = queryEmailFolder(user, password, folderName, messageSendService, store);
-            Integer count = emailFolder.getMessageCount();
-            int totalDeleted = deletEmailFromServer(keyword, emailFolder, count);
-            // expunges the folder to remove messages which are marked deleted
-            emailFolder.close(true);
-            store.close();
-            StringBuilder sb = new StringBuilder();
-            sb.append("共成功删除邮件" + totalDeleted + "封");
-            messageSendService.send(sb.toString(), user, password, "邮件删除完毕");
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        } catch (MessagingException e) {
-            messageSendService.send(e.toString(), user, password, "删除邮件出错");
+                IMAPFolder emailFolder = queryEmailFolder(user, password, folderName, messageSendService, store);
+                Integer count = emailFolder.getMessageCount();
+                int totalDeleted = deletEmailFromServer(keyword, emailFolder, count);
+                // expunges the folder to remove messages which are marked deleted
+                emailFolder.close(true);
+                store.close();
+                StringBuilder sb = new StringBuilder();
+                sb.append("共成功删除邮件" + totalDeleted + "封");
+                messageSendService.send(sb.toString(), user, password, "邮件删除完毕");
+            } catch (NoSuchProviderException e) {
+                e.printStackTrace();
+            } catch (MessagingException e) {
+                messageSendService.send(e.toString(), user, password, "删除邮件出错");
+            }
+            try {
+                Thread.sleep(10000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -53,55 +63,71 @@ public class MailDelete {
      * @return
      * @throws MessagingException
      */
-    private static int deletEmailFromServer(String keyword, IMAPFolder emailFolder, Integer count)
-                                                                                                  throws MessagingException {
+    private static int deletEmailFromServer(final String keyword, final IMAPFolder emailFolder, Integer count)
+                                                                                                              throws MessagingException {
         Calendar calendar = Calendar.getInstance();
         // 当前时间3天之前的邮件
-        calendar.add(Calendar.DAY_OF_MONTH, -3);
-        Date date = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_MONTH, -2);
+        final Date date = calendar.getTime();
         // retrieve the messages from the folder in an array and print it
         int j = 1;
         int x = 0;
+        ExecutorService service = Executors.newFixedThreadPool(50);
         while (j <= count) {
             if (!emailFolder.isOpen()) {
                 emailFolder.open(Folder.READ_WRITE);
             }
             Message[] messages = null;
             try {
-                messages = emailFolder.getMessages(j, (j + 10) > count ? count : (j + 10));
+                messages = emailFolder.getMessages(j, (j + 100) > count ? count : (j + 100));
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("j---" + j);
                 System.out.println("count---" + count);
                 break;
             }
+            final Message[] finalMessages = messages;
+            service.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    int x = 0;
+                    try {
+                        for (int i = 0; i < finalMessages.length; i++) {
+                            if (!emailFolder.isOpen()) {
+
+                                emailFolder.open(Folder.READ_WRITE);
+
+                            }
+                            Message message = finalMessages[i];
+                            System.out.println(Thread.currentThread().getName()+"---------------------------------");
+
+                            Date receivedDate = null;
+                            try {
+                                receivedDate = message.getSentDate();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                continue;
+                            }
+                            if (receivedDate.getTime() < date.getTime()
+                                && message.getFrom()[0].toString().contains(keyword)) {
+                                x++;
+
+                                System.out.println(Thread.currentThread().getName()+"Subject: " + message.getSubject());
+                                message.setFlag(Flags.Flag.DELETED, true);
+                            }
+
+                        }
+                        System.out.println(Thread.currentThread().getName()+":共删除"+x+"封邮件");
+                        emailFolder.close(true);
+                    } catch (MessagingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
             System.out.println("messages.length---" + messages.length);
 
-            for (int i = 0; i < messages.length; i++) {
-                if (!emailFolder.isOpen()) {
-                    emailFolder.open(Folder.READ_WRITE);
-                }
-                Message message = messages[i];
-                System.out.println("---------------------------------");
-
-                Date receivedDate = null;
-                try {
-                    receivedDate = message.getSentDate();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    continue;
-                }
-                if (receivedDate.getTime() < date.getTime() && message.getFrom()[0].toString().contains(keyword)) {
-                    x++;
-                    System.out.println("Subject: " + message.getSubject());
-                    message.setFlag(Flags.Flag.DELETED, true);
-                }
-                if (x % 100 == 0) {
-                    emailFolder.close(true);
-                }
-            }
-
-            j = j + 10;
+            j = j + 100;
         }
         return x;
     }
---
+++ src/SettleBatchBatchCreate.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013-2015 duolabao.com All right reserved. This software is the confidential and proprietary information of
+ * duolabao.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with duolabao.com.
+ */
+package PACKAGE_NAME;
+
+/**
+ * 类SettleBatchBatchCreate.java的实现描述：TODO 类实现描述
+ *
+ * @author baoxing.peng 2016年12月27日 17:05:52
+ */
+public class SettleBatchBatchCreate {
+
+}
